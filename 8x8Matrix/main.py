a1825b594ac536832737c06ea88ac734cccd2fd9
Emati Mitame <gitcommits@mita.me>
2014-12-05 18:38:57 +0000

    Add whole row animation

--- 8x8Matrix/main.py
+++ 8x8Matrix/main.py
@@ -1,13 +1,31 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO #@UnresolvedImport
 import time
 
+frameSpeed = 25
 class pin():
     clock = 18
+    reset = 23
+    input = 24
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin.clock, GPIO.OUT)
-while 1:
-    GPIO.output(pin.clock, GPIO.HIGH)
-    time.sleep(0.25)
-    GPIO.output(pin.clock, GPIO.LOW)
-    time.sleep(0.75)
+GPIO.setup(pin.reset, GPIO.OUT)
+GPIO.setup(pin.input, GPIO.OUT)
+
+f = open("./RowAni")
+frames = f.read().split("\n")
+def tick(on=pin.clock):
+    GPIO.output(on,GPIO.HIGH)
+    GPIO.output(on,GPIO.LOW)
+
+for frame in frames:
+    for l in frame:
+        if l == "1":
+            GPIO.output(pin.input,GPIO.LOW)
+        else:
+            GPIO.output(pin.input,GPIO.HIGH)
+        tick()
+        time.sleep(1/(frameSpeed*len(frame)))
+    tick(pin.reset)
+    
