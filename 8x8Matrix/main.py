e4816c0f98a0d72ee95840b3e6cc6843e3d07078
Emati Mitame <gitcommits@mita.me>
2014-12-05 18:56:00 +0000

    Add transistor output

--- 8x8Matrix/main.py
+++ 8x8Matrix/main.py
@@ -7,11 +7,13 @@ class pin():
     clock = 18
     reset = 23
     input = 24
+    out = 25
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin.clock, GPIO.OUT)
 GPIO.setup(pin.reset, GPIO.OUT)
 GPIO.setup(pin.input, GPIO.OUT)
+GPIO.setup(pin.out, GPIO.OUT)
 
 f = open("./RowAni")
 frames = f.read().split("\n")
@@ -22,10 +24,16 @@ def main():
     for frame in frames:
         for l in frame:
             if l == "1":
-                GPIO.output(pin.input,GPIO.LOW)
+                GPIO.output(pin.out,GPIO.LOW)
             else:
-                GPIO.output(pin.input,GPIO.HIGH)
+                GPIO.output(pin.out,GPIO.HIGH)
             tick()
             time.sleep(1/(frameSpeed*len(frame)))
         tick(pin.reset)
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    GPIO.cleanup()
+        
