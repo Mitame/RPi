642484371a0f2ef89f92b901d3468de36118df38
Emati Mitame <gitcommits@mita.me>
2014-12-09 23:15:11 +0000

    Rename main.py and remove some useless code.
    Prob some other stuff was changed but I can't compare them currently

--- Matrix/run.py
+++ Matrix/run.py
@@ -3,52 +3,68 @@
 import sys
 import time
 import os
-import RPi.GPIO as GPIO  #@UnresolvedImport
+import pygame.time
 
-if os.path.curdir.split("/")[-1] == "Matrix":
-    os.chdir("..")
+try:
+    import RPi.GPIO as GPIO  #@UnresolvedImport
+    rootDir = os.path.abspath("/home/pi/RPi")
+except RuntimeError:
+    rootDir = os.path.abspath("/home/leviwright/Documents/workspace/RPi")
 
-from objects import Frame  #@UnresolvedImport 
+os.chdir(rootDir)
+    
+try:
+    from Matrix.objects import Frame  #@UnresolvedImport
+except ImportError:
+    from objects import Frame  #@UnresolvedImport
 
-GPIO.setmode(GPIO.BCM)
+class pin():
+    #row = [2,3,4,17,27,22,10,9]
+    row = [4,17,27,22,25,10,8,7]
+    col0Clk = 14
+    col0Reset = 15
+    col1Clk = 18
+    col1Reset = 23
+    all = []
+pin.all.extend(pin.row)
+pin.all.extend((pin.columnClk,pin.columnLight,pin.columnReset))
 
+def loadPi():
+    GPIO.setmode(GPIO.BCM)
+    for on in pin.all:
+        GPIO.setup(on,GPIO.OUT)
 
 
 def loadConfig():
-    global fps, overwrite,columns
+    global fps, overwrite,matrixSize
     try:
         f = open("./Matrix/conf").read().strip().split("\n")
         co = {}
         for x in f:
             y = x.split("=")
             co[y[0].lower()] = y[1]
+        print(co)
         fps = int(co["fps"])
         overwrite = int(co["overwrite"])
-        columns = int(co["columns"])
+        matrixSize= tuple(int(x.strip()) for x in co["matrixsize"].strip("()").split(","))
+        ledSize= tuple(int(x.strip()) for x in co["ledsize"].strip("()").split(","))
     except IndexError:
         print((x,y))
-    except:
+    except FileNotFoundError:
         fps = 2
         overwrite = 75
-        columns = 8
+        matrixSize = (8,8)
+        ledSize = (25,25)
+        print(os.path.abspath("./Matrix/conf"))
         f = open("./Matrix/conf","w")
         f.write("fps="+str(fps)+
                 "\noverwrite="+str(overwrite)+
-                "\ncolumns="+str(columns))
+                "\nmatrixsSize="+str(matrixSize)+
+                "\nledSize="+str(ledSize))
         f.close()
+    return fps,overwrite,matrixSize,ledSize
 
-class pin():
-    row = [4,17,27,22,25,10,9,11]
-    columnClk = 18
-    columnLight = 23
-    columnReset = 24
-    all = []
 
-pin.all.extend(pin.row)
-pin.all.extend((pin.columnClk,pin.columnLight,pin.columnReset))
-
-for on in pin.all:
-    GPIO.setup(on,GPIO.OUT)
     
 def pulse(on):
     GPIO.output(on,GPIO.HIGH)
@@ -71,29 +87,13 @@ def renderFrameClass(frame,reverse = True):
                 if frame.get((x,y)):
                     GPIO.output(pin.row[y],GPIO.HIGH)
                     
-            time.sleep(time.time()-startTime+(1/(fps*overwrite*columns)))
+            time.sleep(time.time()-startTime+(1/(fps*overwrite*matrixSize[0])))
 
 def renderFramesList(frames,reverse = True):
     for frm in frames:
-        renderFrameClass(frm,reverse)
-        
-def scrollLeft(frame,filler="0"):
-    newFrame = []
-    for x in frame.split("\n"):
-        newFrame.append(x[1:]+filler)
-    return newFrame
-    
-             
-def scrollText(text):
-    frame = "00000\n00000\n00000\n00000\n00000\n00000\n00000\n00000"
-    for char in text:
-        curColumn = 0
-        charFrame = charset[char]
-        for x in range(len(charFrame.split("\n")[0])):
-            frame[x]
-    
+        renderFrameClass(frm,reverse)        
 
-def importDict(name="./Matrix/letters 5x5.txt"):
+def importDict(name="./Matrix/letters 5x7.txt"):
     newDict={}
     f = open(name).read()
     x = f.split("\n\n")
@@ -103,28 +103,38 @@ def importDict(name="./Matrix/letters 5x5.txt"):
     newDict[" "] = strToFrame("000")
     return newDict
 
-def strToFrame(string):
+def strToFrame(string,forceSize=None):
     xs = string.split("\n")
-    size = len(xs[0]),len(xs)
-    oneline = string.replace("\n","")
+    if forceSize != None:
+        size = forceSize
+    else:
+        size = len(xs[0]),len(xs)
     newFrame = Frame(size, 0)
-    for bit in range(len(oneline)):
-        newFrame[bit] = int(oneline[bit])
+    x,y = 0,0
+    for char in string:
+        if char == "\n":
+            x = 0
+            y += 1
+        else:
+            newFrame.set((x,y), int(char))
+            x += 1
     return newFrame
 
 def importTextAni(name):
     newAni = []
     f = open("Matrix/animations/"+name).read()
     x = f.split("\n\n")
-    for frm in x:
-        newAni.append(strToFrame(frm))
+    if x[0].count("(") != 0:
+        forceSize = tuple(int(z) for z in x[0].strip("()").split(",").strip())
+        for frm in x[1:]:
+            newAni.append(strToFrame(frm,forceSize))
+    else:
+        for frm in x[1:]:
+            newAni.append(strToFrame(frm))
     return newAni
 
-def genTextScrollAni(text,gapBetweenChars=1,startBlank=True):
-    try:
-        charset
-    except NameError:
-        global charset
+def genTextScrollAni(text,charset = None,gapBetweenChars=1,startBlank=True):
+    if type(charset) != dict:
         charset = importDict()
     textl = list(text)
     frames = []
@@ -132,22 +142,24 @@ def genTextScrollAni(text,gapBetweenChars=1,startBlank=True):
     for x in range(len(textl)):
         straightFrame.blit(charset[textl[x]], (x*(charset["A"].size[0]+gapBetweenChars),0))
     if startBlank:
-        for x in range(columns):
-            newFrame = Frame((8,len(pin.row)))
-            newFrame.blit(straightFrame,(columns-x,0),((0,0),(x,len(pin.row))))
+        for x in range(matrixSize[0]):
+            newFrame = Frame((matrixSize[0],len(pin.row)))
+            newFrame.blit(straightFrame,(matrixSize[0]-x,0),((0,0),(x,len(pin.row))))
             frames.append(newFrame)
     for x in range(straightFrame.size[0]):
-        newFrame = Frame((8,len(pin.row)))
+        newFrame = Frame((matrixSize[0],len(pin.row)))
         newFrame.blit(straightFrame,(0,0),((x,0),newFrame.size))
         frames.append(newFrame)
     return frames
          
             
-        
-if __name__ == "__main__":
+def main():
     try:
-        GPIO.setmode(GPIO.BCM)
+        global clock
+        pygame.time.init()
         loadConfig()
+        loadPi()
+        clock = pygame.time.Clock()
         if len(sys.argv) == 1:
             x = genTextScrollAni("Hello, World!")
         else:
@@ -155,6 +167,9 @@ if __name__ == "__main__":
                 x = genTextScrollAni(" ".join(sys.argv[2:]))
             elif sys.argv[1].lower() == "ani":
                 x = importTextAni(" ".join(sys.argv[2:]))
+            elif sys.argv[1].lower() == "test":
+                chrs = importDict()
+                x.genTextScrollAni("Hello, World! This is an alphabet test message." +"".join(list(chrs.keys()).sort()),chrs)
             else:
                 print("Unrecoginsed command: %s"%sys.argv[1])
                 print("Quitting...")
@@ -165,4 +180,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         pass
     pulse(pin.columnReset)
-    GPIO.cleanup()
+    GPIO.cleanup()   
+if __name__ == "__main__": main()
+    
